857795ee2fc059d5a0142bc6763926535ea28496
Mohammad Wasim <wasim.html@gmail.com>
2021-10-25 11:36:23 +0600

    initial commit

---
+++ images/index.php
@@ -0,0 +1,10 @@
+<?php
+/*********************************************************************
+* This script has been released with the aim that it will be useful.
+* This script must not be sold by any client or user of Vasplus Programming Blog.
+* Written by Vasplus Programming Blog
+* Website: www.vasplus.info
+* Email: vasplusblog@gmail.com
+* All Copy Rights Reserved by Vasplus Programming Blog
+***********************************************************************/
+?>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>vasPLUS Programming Blog - Domain Name Checker using Ajax, Jquery and PHP</title>
+
+
+
+
+<!-- Required header files -->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/jquery_1.5.2.js"></script>
+<script type="text/javascript" src="js/post_watermarkinput.js"></script>
+<script type="text/javascript" src="js/vpb_script.js"></script>
+
+
+
+
+</head>
+<body>
+<br clear="all">
+
+<center>
+<div style=" font-family:Verdana, Geneva, sans-serif; font-size:24px;">Domain Name Checker using Ajax, Jquery and PHP</div><br clear="all" /><br clear="all" />
+<font style="font-family:Verdana, Geneva, sans-serif; font-size:12px; color:black;">Please type any domain name of your choice such as <b>vasplus</b>, <b>blog</b>, <b>system</b> etc in the text field provided below to demo the system</font><br clear="all"><br clear="all"><br clear="all">
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Code Begins -->
+<center>
+<div class="vpb_main_wrapper">
+<div style="width:430px; float:left;" align="center">
+<div style="width:300px; float:left;" align="left">
+<input type="text" autocomplete="off" class="vpb_textAreaBoxInputs" id="suggested_names" name="suggested_names" placeholder="Enter a desired domain name here"> </div>
+<div style="width:80px; float:left;" align="left">
+<a href="javascript:void(0);" class="vpb_general_button" onClick="vpb_check_this_domain();">Check</a>
+</div>
+<br clear="all"><div id="vpb_search_status" style="width:380px;float:left; margin-top:20px;" align="left"></div><br clear="all">
+ </div>	<br clear="all">
+ </div>
+</center>
+<!-- Code Ends -->
+
+
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+</center>
+</body>
+</html>
